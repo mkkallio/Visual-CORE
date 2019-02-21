4de95172bd0d2a303826f51cd95b6dca0133e313
Mtk112 <impanaza@hotmail.com>
2019-02-21 06:33:51 +0200

    New leaflet map functionality
    Added sattelite view for the map, added leaflet layer control and
    changed navigation to a sidebar.

--- js/index.js
+++ js/index.js
@@ -1,128 +1,96 @@
 'use strict';
 
-/* Leaflet Map Scripts */
-
-var map = L.map('map').setView([16.9459, 97.9593], 6);
-            L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
-            attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-            maxZoom: 18,
-            id: 'mapbox.streets',
-            accessToken: 'pk.eyJ1IjoibWthbGxpbzIiLCJhIjoiY2pyN3Fha2hyMDBxNzN4cW5sYm12MWkwbyJ9.q1pVLHFRx0Cav6vmyACAYw'
-            }).addTo(map);
 //Variables
-var toggleKayin = false;
-var toggleTownships = false;
-var toggleVillages = false;
-var toggleBasins = false;
-var toggleRivers = false;
-/* Map Functions */
+
+var satellite = L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+maxZoom: 18,
+id: 'mapbox.satellite',
+accessToken: 'pk.eyJ1IjoibWthbGxpbzIiLCJhIjoiY2pyN3Fha2hyMDBxNzN4cW5sYm12MWkwbyJ9.q1pVLHFRx0Cav6vmyACAYw'
+}),
+streets = L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+maxZoom: 18,
+id: 'mapbox.streets',
+accessToken: 'pk.eyJ1IjoibWthbGxpbzIiLCJhIjoiY2pyN3Fha2hyMDBxNzN4cW5sYm12MWkwbyJ9.q1pVLHFRx0Cav6vmyACAYw'
+});
+
+/* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
+function openNav() {
+document.getElementById("navSidebar").style.width = "250px";
+document.getElementById("main").style.marginLeft = "250px";
+document.getElementById("mapSection").style.marginLeft = "250px";
+}
+
+/* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
+function closeNav() {
+document.getElementById("navSidebar").style.width = "0";
+document.getElementById("main").style.marginLeft = "0";
+}
 
 // Displays an popup on the map.
 function kayinState (feature,layer){
-    layer.bindPopup("<h1> Kayin State </h1> <p> You clicked an area inside of Kayin State </p>");
+  layer.bindPopup("<h1> Kayin State </h1> <p> You clicked an area inside of Kayin State </p>");
 };
 //Villages popup
 function kayinVillages (feature,layer){
-    layer.bindPopup("<h1> Village of "+feature.properties.NAME_3+". </h1>");
+  layer.bindPopup("<h1> Village of "+feature.properties.NAME_3+". </h1>");
 };
 //Township popup
 function kayinTownship (feature,layer){
-    layer.bindPopup("<h1> Tonwship: "+feature.properties.VARNAME_2+". </h1>");
+  layer.bindPopup("<h1> Tonwship: "+feature.properties.VARNAME_2+". </h1>");
 };
 //Basin popup
 function kayinBasin (feature,layer){
-    layer.bindPopup("<h1> Basin </h1>");
+  layer.bindPopup("<h1> Basin </h1>");
 };
 
 //rivers popup
 function krivers (feature,layer){
-  layer.bindPopup("<h1> Here's a river </h1>");
+layer.bindPopup("<h1> Here's a river </h1>");
 };
 
 
 //Calls kayinState function when the layer is clicked.
 var kayinLayer = new L.geoJson(Kayin, {
-    onEachFeature: kayinState
-    }).addTo(map);
+  onEachFeature: kayinState
+  });
 
 var kayin_villagesLayer = new L.GeoJSON(Kayin_villages, {
-    onEachFeature: kayinVillages
-    });      
+  onEachFeature: kayinVillages
+  });      
 
 var kayin_townshipsLayer = new L.GeoJSON(Kayin_townships, {
-    onEachFeature: kayinTownship
-    });     
+  onEachFeature: kayinTownship
+  });     
 
 var kayin_basinsLayer = new L.GeoJSON(kayin_basins, {
-    onEachFeature: kayinBasin
-    });
+  onEachFeature: kayinBasin
+  });
 
 var riversLayer = new L.GeoJSON(rivers, {
-    onEachFeature: krivers
-    });
+  onEachFeature: krivers
+  });
 
-/* Toggle Layers Functions */
-function toggleKayinLayer() {
-  if(!toggleKayin) {
-    map.removeLayer(kayinLayer);
-    document.getElementById("kayinBtn").style.background = 'white';
-    document.getElementById("kayinBtn").style.color = 'black'
-  } else {
-    map.addLayer(kayinLayer);
-    document.getElementById("kayinBtn").style.background = '#4CAF50';
-    document.getElementById("kayinBtn").style.color = 'white'
-  }
-  toggleKayin = !toggleKayin;
+var baseMaps = {
+"Satellite": satellite,
+"Streets": streets
 };
 
-function toggleVillagesLayer() {
-  if(!toggleVillages) {
-    map.removeLayer(kayin_villagesLayer);
-    document.getElementById("villagesBtn").style.background = 'white';
-    document.getElementById("villagesBtn").style.color = 'black'
-  } else {
-    map.addLayer(kayin_villagesLayer);
-    document.getElementById("villagesBtn").style.background = '#4CAF50';
-    document.getElementById("villagesBtn").style.color = 'white'
-  }
-  toggleVillages = !toggleVillages;
+var overlayMaps = {
+"Kayin" : kayinLayer,
+"Villages": kayin_villagesLayer,
+"Townships": kayin_townshipsLayer,     
+"Basins": kayin_basinsLayer,
+"Rivers": riversLayer
 };
 
-function toggleTownshipsLayer() {
-  if(!toggleTownships) {
-    map.removeLayer(kayin_townshipsLayer);
-    document.getElementById("townshipsBtn").style.background = 'white';
-    document.getElementById("townshipsBtn").style.color = 'black'
-  } else {
-    map.addLayer(kayin_townshipsLayer);
-    document.getElementById("townshipsBtn").style.background = '#4CAF50';
-    document.getElementById("townshipsBtn").style.color = 'white'
-  }
-  toggleTownships = !toggleTownships;
-};
+var map = L.map('map', {
+  center: [16.9459, 97.9593],
+  zoom: 7,
+  maxZoom: 14,
+  minZoom: 6,
+  layers: [streets, kayinLayer]
+});
 
-function toggleBasinsLayer() {
-  if(!toggleBasins) {
-    map.removeLayer(kayin_basinsLayer);
-    document.getElementById("basinsBtn").style.background = 'white';
-    document.getElementById("basinsBtn").style.color = 'black'
-  } else {
-    map.addLayer(kayin_basinsLayer);
-    document.getElementById("basinsBtn").style.background = '#4CAF50';
-    document.getElementById("basinsBtn").style.color = 'white'
-  }
-  toggleBasins = !toggleBasins;
-};
-
-function toggleRiversLayer() {
-  if(!toggleRivers) {
-    map.removeLayer(riversLayer);
-    document.getElementById("riversBtn").style.background = 'white';
-    document.getElementById("riversBtn").style.color = 'black'
-  } else {
-    map.addLayer(riversLayer);
-    document.getElementById("riversBtn").style.background = '#4CAF50';
-    document.getElementById("riversBtn").style.color = 'white'
-  }
-  toggleRivers = !toggleRivers;
-};
+L.control.layers(baseMaps, overlayMaps).addTo(map);
